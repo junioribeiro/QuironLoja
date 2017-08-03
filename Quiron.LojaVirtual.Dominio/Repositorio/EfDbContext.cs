a9729c9778130b15456cf03397890781778817e5
Avatar <Avatar@DESKTOP-985SDDC>
2017-08-03 14:57:02 -0300

    Criação do Repository com Entity e um controller Produto para teste de conexao

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        /// <summary>
+        /// Propriedade que retorna um lista de todos produtos na base
+        /// </summary>
+        public DbSet<Produto> Produtos { get; set; }
+
+        /// <summary>
+        /// Alterando o Padrão de Nomeclatura do Entity
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //tirando a conversão de nomes padrão do banco EX: produtoes
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //informando ao bamco o padrão de nome da nova tabela
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        /// <summary>
+        /// variavel que manipula os dados do banco acessando o DbSet
+        /// </summary>
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos { get { return _context.Produtos; } }
+    }
+}
